7fb402c2fec0cbef0b0b2a7fa2b3c161102ae615
dannyalharris <39772417+dannyalharris@users.noreply.github.com>
2020-11-09 02:58:44 +0800

    8X8-WEB-APP

---
+++ Web-App-8x8/webapp8x8.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+function ToTapToLightPage(){
+	window.location.replace("TapToLight.html");
+}
+
+function ToTapToTextGenerator(){
+	window.location.replace("TapToTextGenerator.html");
+}
+
+function ToTapToLaunchpad(){
+	window.location.replace("TapToLaunchpad.html");
+}
+
+function ToTapToSettings(){
+	window.location.replace("TapToSettings.html");
+}
+
+function ToTapToHome(){
+	window.location.replace("WebApp8x8.html");
+}
